38a244e38c35ad03099b5d7a16b39927873c2919
Raiyan mrf <140499799+raiyanmrf@users.noreply.github.com>
2026-08-07 07:05:21 +0600

    Update utils.js
    ICON_URL, celsius - farenheiht conversion func and their unit

---
+++ src/asset/utils.js
@@ -0,0 +1,14 @@
+export const WEATHER_URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/`;
+export const DEFAULT_REGION = "dhaka";
+export const DEFAULT_UNIT = "metric";
+export const ICON_URL = `https://github.com/visualcrossing/WeatherIcons/blob/main/PNG/4th%20Set%20-%20Color/`;
+
+export function celsiusToFarenheiht(c) {
+  return (c * 9) / 5 + 32;
+}
+export function farenheihtToCelsius(f) {
+  return ((f - 32) * 5) / 9;
+}
+
+export const CELSIUS = "°C";
+export const FARENHEIHT = "°F";
