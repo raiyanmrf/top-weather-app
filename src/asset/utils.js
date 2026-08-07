25324ff805a508fbf4d0d6fa6d67a3623ea03a8b
Raiyan mrf <140499799+raiyanmrf@users.noreply.github.com>
2026-08-07 07:38:03 +0600

    Update utils.js
    NEW: GIPHY URL

--- src/asset/utils.js
+++ src/asset/utils.js
@@ -2,7 +2,7 @@ export const WEATHER_URL = `https://weather.visualcrossing.com/VisualCrossingWeb
 export const DEFAULT_REGION = "dhaka";
 export const DEFAULT_UNIT = "metric";
 export const ICON_URL = `https://github.com/visualcrossing/WeatherIcons/blob/main/PNG/4th%20Set%20-%20Color/`;
-
+export const GIPHY = `https://api.giphy.com/v1/gifs/translate?api_key=i8k1ueQ3WQHOgryIh2oNFQUc1iCT84TW`;
 export function celsiusToFarenheiht(c) {
   return (c * 9) / 5 + 32;
 }
