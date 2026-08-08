c9cadb196ee14d3863f8135cf86b8c0578c0efe9
Raiyan mrf <140499799+raiyanmrf@users.noreply.github.com>
2026-08-08 08:00:51 +0600

    Update utils.js
    CHANGE: DEFAULT_REGION 'dhaka' -> 'london'

--- src/asset/utils.js
+++ src/asset/utils.js
@@ -1,5 +1,5 @@
 export const WEATHER_URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/`;
-export const DEFAULT_REGION = "dhaka";
+export const DEFAULT_REGION = "london";
 export const DEFAULT_UNIT = "metric";
 export const ICON_URL = `https://github.com/visualcrossing/WeatherIcons/blob/main/PNG/4th%20Set%20-%20Color/`;
 export const GIPHY = `https://api.giphy.com/v1/gifs/translate?api_key=i8k1ueQ3WQHOgryIh2oNFQUc1iCT84TW`;
